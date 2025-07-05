1d5607b2f68cf3c247be833be2b0c44b30deec23
Abel  Mora <131829158+Abel214@users.noreply.github.com>
2025-07-05 12:14:11 -0500

    Delete Sistemas_GestionTareas/backend directory

--- Sistemas_GestionTareas/backend/Calificaciones/controller.js
+++
@@ -1,4 +0,0 @@
-// Controlador para Calificaciones
-exports.getAllCalificaciones = (req, res) => {
-  res.json({ mensaje: 'Lista de calificaciones' });
-};
--- Sistemas_GestionTareas/backend/Tareas/routes.js
+++
@@ -1,9 +0,0 @@
-// Rutas para Tareas
-const express = require('express');
-const router = express.Router();
-const tareasController = require('./controller');
-
-// Ejemplo de ruta
-router.get('/', tareasController.getAllTareas);
-
-module.exports = router;
