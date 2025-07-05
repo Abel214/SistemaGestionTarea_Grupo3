dc9b47ea13bfac2be087f901878d2c34215e4d38
Abel  Mora <131829158+Abel214@users.noreply.github.com>
2025-07-05 12:12:40 -0500

    Delete Sistemas_GestionTareas/backend directory

--- Sistemas_GestionTareas/backend/Registro/routes.js
+++
@@ -1,8 +0,0 @@
-// Rutas para Registro
-const express = require('express');
-const router = express.Router();
-const registroController = require('./controller');
-
-router.get('/', registroController.getAllRegistros);
-
-module.exports = router;
